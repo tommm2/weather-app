0487af371e7d74e82c4faf09bededabe337f4f50
tomm2 <tom875694231@gmail.com>
2021-09-27 18:38:20 +0800

    weather-app

---
+++ README.md
@@ -0,0 +1,14 @@
+# Weather-app
+
+簡介：
+* 使用 OpenWeather API 製作當日國家天氣查詢
+* 使用 SCSS、Bootstrap 排版
+
+功能：
+* 查詢跑出建議列表(Type Ahead)
+* 如果查詢無結果跳出警示訊息
+* 顯示當日溫度、濕度、大氣壓力、風速、日出及日落時間
+
+[DEMO](https://tommm2.github.io/weather-app/#/)
+
+![img](https://i.postimg.cc/DyWbXQtk/image.jpg)
---
+++ script/script.js
@@ -0,0 +1,145 @@
+const search = document.getElementById("search");
+const matchList = document.getElementById("match-list");
+const weahterUI = document.getElementById("weather-ui");
+const alertMsg = document.getElementById("alert-msg");
+
+// popup alert message
+function popupAlert(msg, type = 'warning') {
+  alertMsg.innerHTML = `
+    <div class="alert alert-${type}">
+      <i class="fas fa-exclamation-circle"></i>
+      ${msg}
+      <button type="button" id="btn-close" class="btn-close position-absolute end-0"></button>
+    </div>
+  `;
+  const closeBtn = document.getElementById('btn-close');
+  closeBtn.addEventListener('click', () =>  alertMsg.classList.remove('show'));
+  alertMsg.classList.add('show');
+  setTimeout(() => {
+    alertMsg.classList.remove('show');
+  }, 3000)
+
+  
+}
+
+// fetch own country data
+async function searchStates(searchText) {
+  const res = await fetch("/js練習/weather-app/script/countries.json");
+  const states = await res.json();
+
+  // 取得目前輸入文字後篩選的國家
+  let matches = states.filter((state) => {
+    // 正規表達式，開頭以 "^" 做開始，"gi" 為不管是大寫或小寫
+    const regex = new RegExp(`^${searchText}`, "gi");
+    return state.name.match(regex) || state.code.match(regex);
+  });
+  
+  if (search.value.length === 0) {
+    matches = [];
+    matchList.innerHTML = "";
+  }
+
+  if(matches.length === 0) {
+    matchList.innerHTML = "";
+  }
+
+  updateMatchList(matches);
+}
+// fetch openweatherapi
+async function getCountryWeahter(country) {
+  const api_key = "c0c533d0d20d663add933b306f6b82e4";
+  const api_url = `http://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${api_key}`;
+  const res = await fetch(api_url);
+  const data = await res.json();
+
+  search.value = '';
+  matchList.innerHTML = '';
+  console.log(data);
+  if(data.cod === 200) {
+    updateWeatherUI(data);
+  } else {
+    popupAlert('City not found', 'warning')
+  }
+}
+
+
+// 開氏溫度轉攝氏溫度
+function foramtTemp(k) {
+  const c = Math.floor(k - 273.15);
+  return c; 
+}
+
+// 轉換日出及日落時間
+function convertTime(unixTime) {
+  let dt = new Date(unixTime * 1000);
+  let h = dt.getHours();
+  let m = dt.getMinutes();
+  h = h < 10 ? `0${h}` : h;
+  m = m < 10 ? `0${m}` : m;
+
+  return `${h}:${m}`;
+}
+
+// 更新 match-list DOM
+function updateMatchList(matches) {
+  if (matches.length > 0) {
+    const list = matches.map((match, index) => `
+      <div id="card" class="card card-body" data-country="${match.name}">
+        <h4 class="inline">${match.name} (${match.code})</h4>
+      </div>
+    `).join("");
+    matchList.innerHTML = list;
+  }
+}
+
+// 更新 weahter-ui DOM
+function updateWeatherUI(country) {
+  const { weather, name, main, wind, sys } = country
+  let str = `
+    <div class="content d-flex flex-column justify-content-between text-light">
+      <div class="d-flex justify-content-between align-items-center px-3">
+        ${name}
+        <span><strong class="fs-1">${foramtTemp(main.temp)}</strong>。c</span>
+      </div>
+      <div class="d-flex flex-column justify-content-center align-items-center px-3">
+        <span class="fs-1 d-block">${weather[0].main}</span>
+        <img src="http://openweathermap.org/img/wn/${weather[0].icon}@2x.png" alt="weather-icon">
+      </div>
+      <div class="d-flex justify-content-around align-items-center">
+        <div>
+          <i class="fas fa-sunrise fs-4"></i>
+          ${convertTime(sys.sunrise)}
+        </div>
+        <div>
+          <i class="fas fa-sunset fs-4"></i>
+          ${convertTime(sys.sunset)}
+        </div>
+      </div>
+      <div class="row g-0 text-center bg-dark rounded-bottom py-2">
+        <div class="col-4 border-end border-secondary border-3 py-3">
+        ${main.humidity}%
+          <i class="fas fa-tint d-block mt-2"></i>
+        </div>
+        <div class="col-4 border-end border-secondary border-3 py-3">
+          ${wind.speed}km/h
+          <i class="fas fa-wind d-block mt-2"></i>
+        </div>
+        <div class="col-4 py-3">
+          ${main.pressure / 1000}kPa
+          <i class="fas fa-tachometer-alt d-block mt-2"></i>
+        </div>
+      </div>
+    </div>
+  `
+  weahterUI.innerHTML = str;
+}
+// updateWeatherUI(tempData);
+
+// Event Listener
+search.addEventListener("input", () => searchStates(search.value));
+matchList.addEventListener("click", (e) => {
+  if(e.target.id === 'card') {
+    const country = e.target.dataset.country;
+    getCountryWeahter(country);
+  }
+});
